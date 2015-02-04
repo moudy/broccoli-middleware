26ad5250ced3b7a0389ab57a7e14f78695ed5dd7
Thomas Rix <tom@rixth.org>
2015-02-05 12:50:42 +1300

    Add livereload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var debug = require('debug')('broccoli-middleware');
 var broccoli = require('broccoli');
 var Watcher = require('broccoli/lib/watcher');
 var middleware = require('broccoli/lib/middleware');
@@ -6,5 +7,32 @@ var tree = broccoli.loadBrocfile();
 var builder = new broccoli.Builder(tree);
 var watcher = new Watcher(builder);
 
-module.exports = middleware(watcher);
+module.exports = function (options) {
+  options = options || {};
+
+  if (options.useLiveReload) {
+    var tinylr = require('tiny-lr'),
+        port = options.liveReloadPort || 35729,
+        livereloadServer = new tinylr.Server();
+
+    livereloadServer.listen(port, function (err) {
+      if (err) {
+        throw err;
+      } else {
+        debug('tiny-lr listening on', port);
+      }
+    });
+
+    watcher.on('change', function (results) {
+      debug('deteched change, reloading clients');
+      livereloadServer.changed({
+        body: {
+          files: ['dummy']
+        }
+      });
+    })
+  }
+
+  return middleware(watcher);
+};
 
